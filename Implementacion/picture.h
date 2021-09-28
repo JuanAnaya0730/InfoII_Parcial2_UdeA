b6cfecefd413d3b242a35db1170a4698d9df41f8
Juan Anaya <juan.anayar@udea.edu.co>
2021-09-28 14:30:50 -0500

    se hace inclusion de las librerias a usar

--- Implementacion/picture.h
+++ Implementacion/picture.h
@@ -1,9 +1,18 @@
 #ifndef PICTURE_H
 #define PICTURE_H
 
+#include <iostream>
+#include <QImage>
+#include <QColor>
+#include <QString>
+
+using namespace std;
 
 class Picture
 {
+private:
+    QImage image;
+
 public:
     Picture();
 };
