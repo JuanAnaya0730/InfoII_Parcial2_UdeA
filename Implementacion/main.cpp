e13b9b7b442505393f4028fcad0d2baefdcf58ea
Juan Anaya <juan.anayar@udea.edu.co>
2021-09-28 21:11:26 -0500

    se añadio el algoritmo que gestiona el procesamiento de una imagen

--- Implementacion/main.cpp
+++ Implementacion/main.cpp
@@ -4,5 +4,20 @@ using namespace std;
 
 int main()
 {
+    string fileName;
+    Picture flag;
+
+    cout << "Ingrese el nombre del la imagen: ";
+    cin >> fileName;
+
+    flag.load(fileName.c_str());
+    if(flag.isLoad()){
+        flag.resize(12,12);
+        flag.arduino();
+        cout << "Exito en el proceso." << endl;
+    }else{
+        cout << "No se pudo abrir la imagen " << fileName << endl;
+    }
+
     return 0;
 }
